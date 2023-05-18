2e9d1d881ea6736b2d2982074dbb5e4ef2530231
rezaBG <rezabarzegargashti@gmail.com>
2023-05-18 11:44:37 +0200

    init Meals components and styles

---
+++ src/Meals/MealsSummary.js
@@ -0,0 +1,14 @@
+import clasess from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  <section className={clasess.summary}>
+    <h2>Delicius Food, Delivered To You</h2>
+    <p>choose your favorite meal from our broad selection of Available meals</p>
+    <p>
+      All our meals are cooled with high-qualitiy ingredients, just-in-time and
+      of course by expersienced chefs!
+    </p>
+  </section>;
+};
+
+export default MealsSummary;
